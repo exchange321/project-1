1d80b8517bb7a737e4e5865ada8ad637ce62fae1
exchange321 <chuen_hong@yahoo.com>
2017-04-17 17:02:42 +1000

    Added width and height to thumbnails

--- client/src/components/search/searchResults/SearchResult.jsx
+++ client/src/components/search/searchResults/SearchResult.jsx
@@ -7,7 +7,7 @@ const SearchResult = ({ className, title, description, videoId, thumbnail, handl
   <div className={`search-result clearfix ${className}`} onClick={(e) => handleResultClick(videoId)}>
     <div className="row">
       <div className="thumbnail col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-3">
-        <img className="img-fluid" src={thumbnail} alt={title} />
+        <img className="img-fluid" width="120" height="90" src={thumbnail} alt={title} />
       </div>
       <div className="result-info col-xs-12 col-sm-12 col-md-12 col-lg-8 col-xl-9">
         <div className="result-title">
