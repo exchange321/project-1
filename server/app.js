9c7b37562e65a7b11cc20ed38e925596f0f32769
exchange321 <adchuenhong@gmail.com>
2017-05-12 13:17:23 +1000

    Ready to deploy

--- server/app.js
+++ server/app.js
@@ -7,7 +7,10 @@ const compress = require('compression');
 const cors = require('cors');
 const feathers = require('feathers');
 const configuration = require('feathers-configuration');
-const hooks = require('feathers-hooks');const socketio = require('feathers-socketio');
+const hooks = require('feathers-hooks');
+const rest = require('feathers-rest');
+const bodyParser = require('body-parser');
+const socketio = require('feathers-socketio');
 const middleware = require('./middleware');
 const services = require('./services');
 
@@ -18,11 +21,25 @@ app.configure(configuration(path.join(__dirname, '..')));
 app.use(compress())
   .options('*', cors())
   .use(cors())
-  .use(favicon( path.join(app.get('public'), 'favicon.ico') ))
-  .use('/', serveStatic( app.get('public') ))
+  .use(favicon( path.join(app.get('assets'), 'favicon.ico') ))
+  .use('/', serveStatic(app.get('public')))
+  .use('/assets', serveStatic(app.get('assets')))
+  .use('/login', serveHtml)
+  .use('/logout', serveHtml)
+  .use('/watch', serveHtml)
+  .use('/favorite', serveHtml)
+  .use('/history', serveHtml)
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
   .configure(hooks())
+  .configure(rest())
   .configure(socketio())
   .configure(services)
   .configure(middleware);
 
 module.exports = app;
+
+function serveHtml(req, res) {
+  const html = path.join('.', 'index.html');
+  res.sendFile(html, { root: 'public' });
+}
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,3 +1,56 @@
-/**
- * Created by Wayuki on 24-Mar-17.
- */
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const GLOBALS = {
+  'process.env.NODE_ENV': JSON.stringify('production'),
+};
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    'whatwg-fetch',
+    `${__dirname}/client/index.js`,
+  ],
+  target: 'web',
+  output: {
+    path: `${__dirname}/public`, // Note: Physical files are only output by the production build task `npm run build`.
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract(
+          [
+            'css-loader?sourceMap',
+            'postcss-loader',
+          ]
+        ),
+      },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
+      { test: /\.(woff|woff2)$/, loader: 'url-loader?prefix=font/&limit=5000' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin(GLOBALS),
+    new ExtractTextPlugin('index.css'),
+    new webpack.optimize.UglifyJsPlugin(),
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/client/index.html`,
+      alwaysWriteToDisk: true,
+    }),
+    new HtmlWebpackHarddiskPlugin(),
+  ],
+};
